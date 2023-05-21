7ecb8178e8fc03b2830c274ca2ad752bdb51e7e0
srajabzadeh <sara.rajabzadeh@sorint.it>
2023-05-21 10:31:53 +0200

    bubblesort in python

---
+++ bubblesort/bubblesort.py
@@ -0,0 +1,13 @@
+def bubblesort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+def main():
+        arr = [64, 34, 25, 12, 22, 11, 90]
+        bubblesort(arr)
+        print(arr)
+if __name__ == "__main__":
+    main()                    
